d3f9e456cf1cbae79537e04adb6184e74f298119
PHFM80 <101189358+PHFM80@users.noreply.github.com>
2022-04-13 18:20:48 -0300

    13/04__18:20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { DatospersonalesComponent } from './components/datospersonales/datospersonales.component';
 import { FotopabloComponent } from './components/fotopablo/fotopablo.component';
 import { FotoportadaComponent } from './components/fotoportada/fotoportada.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogoComponent } from './components/navbar/logo/logo.component';
+import { MenuComponent } from './components/navbar/menu/menu.component';
+import { LoginComponent } from './components/navbar/login/login.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,11 @@ import { FotoportadaComponent } from './components/fotoportada/fotoportada.compo
     HeaderComponent,
     DatospersonalesComponent,
     FotopabloComponent,
-    FotoportadaComponent
+    FotoportadaComponent,
+    NavbarComponent,
+    LogoComponent,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
